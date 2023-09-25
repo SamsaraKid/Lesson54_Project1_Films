b9016caabfda6add2b5747e65701aa37c9297890
koany <koroliev@yandex.ru>
2023-09-25 18:46:20 +0300

    выбор вида и пород

--- catalog/models.py
+++ catalog/models.py
@@ -2,14 +2,20 @@ from django.db import models
 
 
 class Kind(models.Model):
-    name = models.CharField(max_length=20)
+    choise = (('кот', 'кот'), ('собака', 'собака'), ('птица', 'птица'), ('рептилия', 'рептилия'), ('грызун', 'грызун'))
+    name = models.CharField(max_length=20, choices=choise)
 
     def __str__(self):
         return self.name
 
 
 class Breed(models.Model):
-    name = models.CharField(max_length=50)
+    choise = (('сибирская', 'сибирская'), ('британская', 'британская'),
+              ('лайка', 'лайка'), ('овчарка', 'овчарка'),
+              ('попугай', 'попугай'), ('канарейка', 'канарейка'),
+              ('черепаха', 'черепаха'), ('ящерица', 'ящерица'),
+              ('хомяк', 'хомяк'), ('мышь', 'мышь'))
+    name = models.CharField(max_length=50, choices=choise)
     kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
 
     def __str__(self):
