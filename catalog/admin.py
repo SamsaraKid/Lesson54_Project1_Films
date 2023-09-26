587163d0aa375e1a6908fe237bd1ddb1296d7a6e
koany <koroliev@yandex.ru>
2023-09-26 21:00:41 +0300

    добавил админку и заполнил данные

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Diagnosis)
+admin.site.register(Medicines)
+admin.site.register(Doctor)
+
+
+class Patientadmin(admin.ModelAdmin):
+    list_display = ('name', 'kind', 'breed', 'diags')
+    list_filter = ('kind', 'diag', 'meds', 'doc')
+    fieldsets = (('Информация', {'fields': ('name', 'kind', 'breed')}),
+                 ('Лечение', {'fields': ('doc', 'diag', 'meds')}))
+admin.site.register(Patient, Patientadmin)
+
+
+class Breedadmin(admin.ModelAdmin):
+    list_display = ('name', 'kind')
+admin.site.register(Breed, Breedadmin)
+
+
+class Stinline(admin.TabularInline):
+    model = Patient
+
+
+class Kindadmin(admin.ModelAdmin):
+    inlines = [Stinline]
+admin.site.register(Kind, Kindadmin)
+
--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Kind(models.Model):
     choise = (('кот', 'кот'), ('собака', 'собака'), ('птица', 'птица'), ('рептилия', 'рептилия'), ('грызун', 'грызун'))
-    name = models.CharField(max_length=20, choices=choise)
+    name = models.CharField(max_length=20, choices=choise, verbose_name='Вид')
 
     def __str__(self):
         return self.name
@@ -15,30 +15,46 @@ class Breed(models.Model):
               ('попугай', 'попугай'), ('канарейка', 'канарейка'),
               ('черепаха', 'черепаха'), ('ящерица', 'ящерица'),
               ('хомяк', 'хомяк'), ('мышь', 'мышь'))
-    name = models.CharField(max_length=50, choices=choise)
-    kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, choices=choise, verbose_name='Разновидность')
+    kind = models.ForeignKey(Kind, on_delete=models.CASCADE, verbose_name='Вид')
 
     def __str__(self):
         return self.name
 
 
 class Diagnosis(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Диагноз')
 
     def __str__(self):
         return self.name
 
 
 class Medicines(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Лекарства')
 
     def __str__(self):
         return self.name
 
 
+class Doctor(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Врач')
+
+    def __str__(self):
+        return self.name
+
 class Patient(models.Model):
-    name = models.CharField(max_length=20)
-    kind = models.ForeignKey(Kind, on_delete=models.SET_NULL, null=True)
-    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True)
-    diag = models.ManyToManyField(Diagnosis, null=True)
-    meds = models.ManyToManyField(Medicines, null=True)
+    name = models.CharField(max_length=20, verbose_name='Кличка')
+    kind = models.ForeignKey(Kind, on_delete=models.SET_NULL, null=True, verbose_name='Вид')
+    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True, verbose_name='Разновидность')
+    diag = models.ManyToManyField(Diagnosis, null=True, verbose_name='Диагноз')
+    meds = models.ManyToManyField(Medicines, null=True, verbose_name='Лекарства')
+    doc = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, verbose_name='Врач')
+
+    def __str__(self):
+        return self.name
+
+    def diags(self):
+        return ', '.join([i.name for i in self.diag.all()])
+
+    def medses(self):
+        return ', '.join([i.name for i in self.meds.all()])
